897eed0457eeeacef6dd57be7b8a3e6cae044f43
Koriťáková <150925689+VceraToFungovalo@users.noreply.github.com>
2024-01-01 19:02:12 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask
 from . import db
 
 app = Flask(__name__)
@@ -20,7 +20,7 @@ db.init_app(app)
 @app.route('/')
 def hello_world():  # put application's code here
     return "Hello TdA"
-
+@app.route('/data', methods=['GET'])
 
 if __name__ == '__main__':
     app.run(debug=True)
